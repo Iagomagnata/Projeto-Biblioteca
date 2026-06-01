465c3fb431a7fb88427ba5a6f3d2e40e9ea5f9d0
esmer4lda <rebekinhalara09@gmail.com>
2026-06-01 13:11:57 -0300

    332333323

--- js/Script.js
+++ js/Script.js
@@ -14,40 +14,18 @@ async function carregarUsuarios() {
 }
 
 function cadastrarUsuario() {
-    let nome = document.getElementById('nome').value;
-    let nomeSocial = document.getElementById('nomeSocial').value;
-    let email = document.getElementById('email').value;
-    let dataNascimento = document.getElementById('dataNascimento').value;
-    let genero = document.getElementById('genero').value;
-    let endereco = document.getElementById('endereco').value;
-    let numero = document.getElementById('numero').value;
-    let bairro = document.getElementById('bairro').value;
-    let cidade = document.getElementById('cidade').value;
-    let cep = document.getElementById('cep').value;
-    let telefone = document.getElementById('telefone').value;
-    let telefoneResponsavel = document.getElementById('telefoneresponsavel').value;
-    let cep = document.getElementById('cep').value;
+   let matricula = document.getElementById('Matricula').value;
+   let email = document.getElementById('email').value;
 
-    if (!nome || !email || !dataNascimento || !genero || !endereco || !numero || !bairro || !cidade || !cep || !telefone || !telefoneResponsavel) {
+    if (!matricula || !email) {
         alert('Preencha todos os campos obrigatórios!');
         return;
     }
 
     const novoUsuario = {
-        nome: nome,
-        nomeSocial: nomeSocial,
         email: email,
-        dataNascimento: dataNascimento,
-        genero: genero,
-        endereco: endereco,
-        numero: numero,
-        bairro: bairro,
-        cidade: cidade,
-        cep: cep,
-        telefone: telefone,
-        telefoneResponsavel: telefoneResponsavel
-    };
-    
+        matricula: matricula
+        };
     console.log('url: ', api_url);
 
     try {
@@ -77,20 +55,8 @@ function cadastrarUsuario() {
 } 
 
 function limparCampos() {
-    document.getElementById('matricula').value = '';
-    document.getElementById('nome').value = '';
-    document.getElementById('nomeSocial').value = '';
     document.getElementById('email').value = '';
-    document.getElementById('dataNascimento').value = '';
-    document.getElementById('genero').value = '';
-    document.getElementById('endereco').value = '';
-    document.getElementById('numero').value = '';
-    document.getElementById('bairro').value = '';
-    document.getElementById('cidade').value = '';
-    document.getElementById('cep').value = '';
-    document.getElementById('telefone').value = '';
-    document.getElementById('telefoneresponsavel').value = '';
-}
+    document.getElementById('Matricula').value = '';
 
 window.addEventListener('load', carregarUsuarios);
 
@@ -146,4 +112,4 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     });
 });
-
+}
